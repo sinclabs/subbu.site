db8d6612a07cf88fd5ca76a994ca8be2b3ab79fa
Subramanian Alagappan <s.alagappan@klarna.com>
2022-02-13 12:46:20 +0100

    Added linting and fixed tabSize to 2

--- components/ContactLine.tsx
+++ components/ContactLine.tsx
@@ -7,44 +7,44 @@ import { LinkedInLogo } from './LinkedInLogo';
 import { mediaQuerySizes } from '../utils/constants'
 
 type Props = {
-    email?: string
-    phone?: string
-    linkedIn?: string
-    github?: string
+  email?: string
+  phone?: string
+  linkedIn?: string
+  github?: string
 }
 
 const ContactLineContainer = styled.div`
-    width: min(100%, 300px);
-    display: flex;
-    justify-content: space-evenly;
+  width: min(100%, 300px);
+  display: flex;
+  justify-content: space-evenly;
 `
 
 const Link = styled.a`
-    width: 18pt;
-    height: 18pt;
+  width: 18pt;
+  height: 18pt;
 
-    @media (max-width: ${mediaQuerySizes.mobileL}) {
-        width: 12pt;
-        height: 12pt;
-    }
+  @media (max-width: ${mediaQuerySizes.mobileL}) {
+    width: 12pt;
+    height: 12pt;
+  }
 `
 
 const ContactLine: React.FunctionComponent<Props> = ({
-    email = 'subala@kth.se',
-    linkedIn = 'https://www.linkedin.com/in/subramanianalagappan/',
-    github = 'https://github.com/sinclabs',
+  email = 'subala@kth.se',
+  linkedIn = 'https://www.linkedin.com/in/subramanianalagappan/',
+  github = 'https://github.com/sinclabs',
 }) => (
-    <ContactLineContainer>
-        <Link href={github} target="_blank">
-            <GithubLogo />
-        </Link>
-        <Link href={`mailto:${email}`} target="_blank">
-            <EmailLogo />
-        </Link>
-        <Link href={linkedIn} target="_blank">
-            <LinkedInLogo />
-        </Link>
-    </ContactLineContainer>
+  <ContactLineContainer>
+    <Link href={github} target="_blank">
+      <GithubLogo />
+    </Link>
+    <Link href={`mailto:${email}`} target="_blank">
+      <EmailLogo />
+    </Link>
+    <Link href={linkedIn} target="_blank">
+      <LinkedInLogo />
+    </Link>
+  </ContactLineContainer>
 )
 
 export default ContactLine
--- components/Title.tsx
+++ components/Title.tsx
@@ -4,90 +4,90 @@ import styled from 'styled-components'
 import { mediaQuerySizes } from '../utils/constants'
 
 type Props = {
-    firstName?: string
-    lastName?: string
-    nickName?: string
-    topTitle?: string
-    bottomTitle?: string
+  firstName?: string
+  lastName?: string
+  nickName?: string
+  topTitle?: string
+  bottomTitle?: string
 }
 
 const NameWrapper = styled.div`
-    @import url('https://fonts.googleapis.com/css?family=Roboto+Mono:400,400i&display=swap');
-    @font-face{ 
-        font-family: 'basictitlefont';
-        src: url('/fonts/basictitlefont/basictitlefont-webfont.woff') format('woff');
-    }
-    font-family: "basictitlefont", sans-serif;
-    font-size: max(8vw, 1em);
-    margin: auto;
-    width: fit-content;
-    margin-bottom: 50px;
-    transform: skew(0deg, -7deg);
-    display: flex;
-    flex-direction: column;
+  @import url('https://fonts.googleapis.com/css?family=Roboto+Mono:400,400i&display=swap');
+  @font-face{ 
+    font-family: 'basictitlefont';
+    src: url('/fonts/basictitlefont/basictitlefont-webfont.woff') format('woff');
+  }
+  font-family: "basictitlefont", sans-serif;
+  font-size: max(8vw, 1em);
+  margin: auto;
+  width: fit-content;
+  margin-bottom: 50px;
+  transform: skew(0deg, -7deg);
+  display: flex;
+  flex-direction: column;
 
-    @media (max-width: ${mediaQuerySizes.mobileL}) {
-        font-size: max(9.5vw, 1em);
-    }
+  @media (max-width: ${mediaQuerySizes.mobileL}) {
+    font-size: max(9.5vw, 1em);
+  }
 `
 
 const NameContainer = styled.div`
-    display: inline-block;
-    border-top: 3px solid black;
-    border-bottom: 3px solid black;
+  display: inline-block;
+  border-top: 3px solid black;
+  border-bottom: 3px solid black;
 
-    @media (max-width: ${mediaQuerySizes.mobileL}) {
-        border-top: 1px solid black;
-        border-bottom: 1px solid black;
-    }
+  @media (max-width: ${mediaQuerySizes.mobileL}) {
+    border-top: 1px solid black;
+    border-bottom: 1px solid black;
+  }
 `
 
 const FirstName = styled.span`
-    margin: auto;
-    font-weight: 300;
+  margin: auto;
+  font-weight: 300;
 `
 
 const LastName = styled.span`
-    margin: auto;
-    font-weight: 600;
+  margin: auto;
+  font-weight: 600;
 `
 
 const TopTitle = styled.span`
-    font-size: max(4vw, 0.5em);
-    letter-spacing: 5px;
+  font-size: max(4vw, 0.5em);
+  letter-spacing: 5px;
 
-    @media (max-width: ${mediaQuerySizes.mobileL}) {
-        font-size: max(5vw, 0.5em);
-        letter-spacing: 3px;
-    }
+  @media (max-width: ${mediaQuerySizes.mobileL}) {
+    font-size: max(5vw, 0.5em);
+    letter-spacing: 3px;
+  }
 `
 
 const BottomTitle = styled.span`
-    align-self: flex-end;
-    font-size: max(4vw, 0.5em);
-    letter-spacing: 5px;
+  align-self: flex-end;
+  font-size: max(4vw, 0.5em);
+  letter-spacing: 5px;
 
-    @media (max-width: ${mediaQuerySizes.mobileL}) {
-        font-size: max(5vw, 0.5em);
-        letter-spacing: 3px;
-    }
+  @media (max-width: ${mediaQuerySizes.mobileL}) {
+    font-size: max(5vw, 0.5em);
+    letter-spacing: 3px;
+  }
 `
 
 const Title: React.FunctionComponent<Props> = ({
-    firstName = 'Subramanian',
-    lastName = 'Alagappan',
-    // nickName = 'Subbu',
-    topTitle = 'Software Engineer',
-    bottomTitle = 'Interaction Designer',
+  firstName = 'Subramanian',
+  lastName = 'Alagappan',
+  // nickName = 'Subbu',
+  topTitle = 'Software Engineer',
+  bottomTitle = 'Interaction Designer',
 }) => (
-    <NameWrapper>
-        <TopTitle>{topTitle}</TopTitle>
-        <NameContainer>
-            <FirstName>{firstName}</FirstName>
-            <LastName>{` ${lastName}`}</LastName>
-        </NameContainer>
-        <BottomTitle>{bottomTitle}</BottomTitle>
-    </NameWrapper>
+  <NameWrapper>
+    <TopTitle>{topTitle}</TopTitle>
+    <NameContainer>
+      <FirstName>{firstName}</FirstName>
+      <LastName>{` ${lastName}`}</LastName>
+    </NameContainer>
+    <BottomTitle>{bottomTitle}</BottomTitle>
+  </NameWrapper>
 )
 
 export default Title
